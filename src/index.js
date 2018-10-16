0a8b7cdd667f0e4bcacb3b357e6b5dca356f808e
Dima Kurbatov <dskurbatov13@gmail.com>
2018-10-16 16:13:40 -0700

    set up basic js and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <h1>This is React</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
