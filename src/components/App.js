524877cca5e484ec5e337882374bff86ffa18404
Dima Kurbatov <dskurbatov13@gmail.com>
2018-10-17 13:26:17 -0700

    centered caption and removed vertical scroll

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends React.Component{
     return (
       <div className="container">
         {this.props.images.map((image, idx) => {
-          return (<figure>
+          return (<figure key={idx}>
             <img src={image.url} alt={`image ${idx + 1}`} />
             <figcaption>{image.caption}</figcaption>
           </figure>)
